563eb87355ae33ad193fd44cd7c4d95e0e1b6582
Peng <wpcfan@gmail.com>
2024-06-06 21:44:59 +0000

    refactor the names

--- custom_components/jd_smart/api.py
+++ custom_components/jd_smart/api.py
@@ -8,23 +8,23 @@ import aiohttp
 import async_timeout
 
 
-class IntegrationBlueprintApiClientError(Exception):
+class JdSmartApiClientError(Exception):
     """Exception to indicate a general API error."""
 
 
-class IntegrationBlueprintApiClientCommunicationError(
-    IntegrationBlueprintApiClientError
+class JdSmartApiClientCommunicationError(
+    JdSmartApiClientError
 ):
     """Exception to indicate a communication error."""
 
 
-class IntegrationBlueprintApiClientAuthenticationError(
-    IntegrationBlueprintApiClientError
+class JdSmartApiClientAuthenticationError(
+    JdSmartApiClientError
 ):
     """Exception to indicate an authentication error."""
 
 
-class IntegrationBlueprintApiClient:
+class JdSmartApiClient:
     """Sample API Client."""
 
     def __init__(
@@ -70,21 +70,21 @@ class IntegrationBlueprintApiClient:
                     json=data,
                 )
                 if response.status in (401, 403):
-                    raise IntegrationBlueprintApiClientAuthenticationError(
+                    raise JdSmartApiClientAuthenticationError(
                         "Invalid credentials",
                     )
                 response.raise_for_status()
                 return await response.json()
 
         except asyncio.TimeoutError as exception:
-            raise IntegrationBlueprintApiClientCommunicationError(
+            raise JdSmartApiClientCommunicationError(
                 "Timeout error fetching information",
             ) from exception
         except (aiohttp.ClientError, socket.gaierror) as exception:
-            raise IntegrationBlueprintApiClientCommunicationError(
+            raise JdSmartApiClientCommunicationError(
                 "Error fetching information",
             ) from exception
         except Exception as exception:  # pylint: disable=broad-except
-            raise IntegrationBlueprintApiClientError(
+            raise JdSmartApiClientError(
                 "Something really wrong happened!"
             ) from exception
--- custom_components/jd_smart/const.py
+++ custom_components/jd_smart/const.py
@@ -3,7 +3,7 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
-VERSION = "0.0.0"
+NAME = "Jd Smart Integration for Home Assistant"
+DOMAIN = "jd_smart"
+VERSION = "1.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
